3f8bac55be96b1380f0c73dc463b970aef75cc53
Jimbo Freedman <jimbo@project-sapiens.com>
2019-02-18 14:01:29 +0000

    Very basic API to read TodoItems

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+# from django.urls import url, include
+from rest_framework.routers import DefaultRouter
 
 from .views import index, complete, cancel, fail
+from .viewsets import TodoItemViewSet
 
 app_name = "todo"
+
+router = DefaultRouter()
+router.register(r'todo_items', TodoItemViewSet, base_name='todo_items')
+
 urlpatterns = [
     path(r'complete/<int:item_id>/', view=complete, name="complete"),
     path(r'cancel/<int:item_id>/', view=cancel, name="cancel"),
     path(r'fail/<int:item_id>/', view=fail, name="fail"),
     path('', view=index, name="index"),
+    path(r'api/', include(router.urls)),
 ]
